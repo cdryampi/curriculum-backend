f8d72feed12fd64b0031c4b79f826fff938dbe84
cdryampi <cdryampi@gmail.com>
2025-01-27 08:38:06 +0100

    Add user profile metadata and keywords serialization, and implement user profile list view

--- cv_backend/base_user/urls.py
+++ cv_backend/base_user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserProfileDetailView
+from .views import UserProfileDetailView, UserProfileListView
 
 urlpatterns = [
-    path('userprofile/', UserProfileDetailView.as_view(), name='userprofile-detail'),
+    path('userprofile/<int:id>/', UserProfileDetailView.as_view(), name='userprofile-detail'),
+    path('userprofile/', UserProfileListView.as_view(), name='userprofile-list'),
 ]
