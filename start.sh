4c8bdf8c0ebe1bcd9ec73bdfa638510c0d9ca005
cdryampi <cdryampi@gmail.com>
2025-01-29 12:54:25 +0100

    Fix gunicorn command and requirements path in start.sh for correct application reference

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-cd cv_backend
-pip install --no-cache-dir -r requirements.txt
-sh pipeline.sh
-gunicorn cv_backend.wsgi:application --log-file -
+pip install --no-cache-dir -r /cv_backend/cv_backend/requirements.txt
+gunicorn cv_backend.cv_backend.wsgi:application --log-file -
