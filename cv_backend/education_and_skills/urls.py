9234065ffb888794a02f2fc3e84660539f3c549b
cdryampi <cdryampi@gmail.com>
2025-01-31 12:48:32 +0100

    Remove unused files and code across various modules

--- cv_backend/education_and_skills/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CourseView, EducationView, SkillView, SkillViewFilter
-
-urlpatterns = [
-    path('course_list/', CourseView.as_view(), name='course-list'),
-    path('education_list/', EducationView.as_view(), name='education-list'),
-    path('skill_list/', SkillView.as_view(), name='skill-list'),
-    path('skill_list_category/<slug:slug>/', SkillViewFilter.as_view(), name='skill-list-vategory'),
-]
--- cv_backend/redes_sociales/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import SocialMediaProfileDetailView
-
-urlpatterns = [
-    path('social_links/', SocialMediaProfileDetailView.as_view(), name='social-detail'),
-]
